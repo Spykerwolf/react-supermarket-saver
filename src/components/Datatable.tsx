dd64cfc420f6d47e55b88845f331377a6e41cc1c
Will <willsmal.nz@gmail.com>
2024-03-15 14:47:03 +1300

    update Datatable.tsx

--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -17,7 +17,7 @@ import Chip from "@mui/material/Chip";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import RemoveIcon from "@mui/icons-material/Remove";
-import { Button, ButtonGroup } from "@mui/material";
+import { Button, ButtonGroup, getLinkUtilityClass } from "@mui/material";
 import CapitalizeFirstLetter from "./functions/capitalizeFirstLetter";
 import { getComparator, stableSort } from "./functions/sortTable";
 import { newworldSecretToken, paknsaveSecretToken } from "../secrets";
@@ -59,7 +59,6 @@ export function createData(
     standardPrice,
     productPackage,
     ratio,
-
     store,
     URL,
   };
@@ -182,12 +181,12 @@ export default function EnhancedTable() {
   const [orderBy, setOrderBy] = useState<keyof Data>("ratio");
   const [selected, setSelected] = useState<readonly string[]>([]);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState("75");
+  const [rowsPerPage, setRowsPerPage] = useState(75);
   const [countdownResults, setcountdownResults] = useState([]);
   const [countdownAPIStatus, setCountdownAPIStatus] = useState("");
   const [newworldResults, setnewworldResults] = useState([]);
   const [newworldAPIStatus, setNewworldAPIStatus] = useState("");
-  const [newworldProductIDs, setNewworldProductIDs] = useState([]);
+  const [newworldProductSKUs, setNewworldProductSKUs] = useState([]);
   const [paknsaveResults, setpaknsaveResults] = useState([]);
   const [paknsaveAPIStatus, setPaknsaveAPIStatus] = useState("");
   const [filterSearchText, setFilterSearchText] = useState("");
@@ -197,9 +196,124 @@ export default function EnhancedTable() {
   );
   const [searchHelperText, setSearchHelperText] = useState("");
   const [chipData, setChipData] = React.useState([]);
+  const productIdTogether: string[] = [];
+
+  useEffect(() => {
+    // async function displaySKUs() {
+    //   newworldProductSKUs.length &&
+    //     console.log("newworldProductSKUs", newworldProductSKUs);
+    // }
+
+    async function extractSKUs() {
+      newworldProductSKUs !== undefined &&
+        newworldProductSKUs.forEach((product) => {
+          productIdTogether.push(product["productID"]);
+        });
+    }
+
+    // async function displayProductIds() {
+    //   productIdTogether.length &&
+    //     console.log("productIdTogether", productIdTogether);
+    // }
+
+    async function getData() {
+      const combineNewworldSKUsWithProducts: Response = await fetch(
+        `https://api-prod.newworld.co.nz/v1/edge/store/0f82d3fe-acd0-4e98-b3e7-fbabbf8b8ef5/decorateProducts`,
+        {
+          method: "post",
+          headers: new Headers({
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            Authorization: newworldSecretToken,
+            "Content-Type": "application/json",
+          }),
+          body: JSON.stringify({ productIds: productIdTogether }),
+        }
+      );
+      const newworldSKUsJSON = await combineNewworldSKUsWithProducts.json();
+      setnewworldResults(newworldSKUsJSON.products);
+      // console.log("newworldSKUsJSON.products", newworldSKUsJSON.products);
+    }
+
+    async function displayResults() {
+      console.log("starting displayResults");
+      newworldResults !== undefined &&
+        newworldResults.forEach((product, countdownIndex) => {
+          const productName = product["brand"]
+            ? `${product["brand"]} ${product["name"]}`
+            : product["name"];
+          if (
+            productName
+              .toLowerCase()
+              .replace("-", " ")
+              .includes(searchTerm.toLowerCase())
+          ) {
+            const store = "New World";
+            const productStandardPrice = (
+              product["singlePrice"]["price"] / 100
+            ).toFixed(2);
+
+            const productSpecialPrice = product["promotions"]
+              ? (product["promotions"][0]["rewardValue"] / 100).toFixed(2)
+              : productStandardPrice;
+            const productSku = product["productId"];
+
+            const productCupPrice = product["singlePrice"]["comparativePrice"]
+              ? product["singlePrice"]["comparativePrice"]["pricePerUnit"]
+              : "";
+
+            const productCupUnit = product["singlePrice"]["comparativePrice"]
+              ? product["singlePrice"]["comparativePrice"]["unitQuantity"]
+              : "";
+            const productCupMeasure = product["singlePrice"]["comparativePrice"]
+              ? product["singlePrice"]["comparativePrice"]["unitQuantityUom"]
+              : "";
+
+            const ratio =
+              productCupMeasure &&
+              `$${(productCupPrice / 100).toFixed(
+                2
+              )} / ${productCupUnit} ${productCupMeasure
+                ?.replace("l", "L")
+                ?.replace("mL", "ml")}`;
+
+            const productPackage = `${product["displayName"]
+              ?.replace("l", "L")
+              ?.replace("mL", "ml")}`;
+            const URL = `https://www.newworld.co.nz/shop/product/${productSku?.replace(
+              "-",
+              "_"
+            )}`;
+            const onSpecial = product["promotions"] && true;
+            rows.push(
+              createData(
+                countdownIndex,
+                CapitalizeFirstLetter(productName),
+                onSpecial,
+                productSpecialPrice,
+                productStandardPrice,
+                productPackage,
+                ratio,
+                store,
+                URL
+              )
+            );
+          }
+        });
+      console.log("rows", rows);
+    }
+
+    // displaySKUs();
+    extractSKUs();
+    // displayProductIds();
+    newworldProductSKUs.length && getData();
+    newworldResults.length && displayResults();
+  }, [newworldProductSKUs]);
 
   useEffect(() => {}, [countdownResults]);
-  useEffect(() => {}, [newworldResults]);
+  useEffect(() => {
+    newworldResults.length && console.log("newworldResults", newworldResults);
+  }, [rows]);
   useEffect(() => {}, [paknsaveResults]);
   useEffect(() => {
     {
@@ -208,149 +322,43 @@ export default function EnhancedTable() {
     }
   }, [chipData]);
 
-  async function GetData() {
-    const newworldStoreId: string = "0f82d3fe-acd0-4e98-b3e7-fbabbf8b8ef5"; // Orewa
+  async function GetSupermarketPrices() {
+    newworld();
 
-    const fetchNewWorldSkus: Response = await fetch(
-      `http://localhost:8585/https://api-prod.newworld.co.nz/v1/edge/search/products/query/index/products-index-popularity-asc`,
-      {
-        method: "post",
-        headers: new Headers({
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-          Authorization: newworldSecretToken,
-          "Content-Type": "application/json",
-        }),
-        body: JSON.stringify({
-          algoliaQuery: {
-            attributesToHighlight: [],
-            attributesToRetrieve: ["productID", "Type", "sponsored"],
-            facetFilters: [["tobacco:false"]],
-            facets: [
-              "brand",
-              "category2NI",
-              "onPromotion",
-              "productFacets",
-              "tobacco",
-            ],
-            filters: `stores:${newworldStoreId}`,
-            highlightPostTag: "__/ais-highlight__",
-            highlightPreTag: "__ais-highlight__",
-            hitsPerPage: 50,
-            maxValuesPerFacet: 100,
-            page: 0,
-            query: searchTerm,
-          },
-          adDomain: "SEARCH_PAGE",
-          disableAds: false,
-          publishImpressionEvent: true,
-        }),
+    async function newworld() {
+      const storeID: string = "0f82d3fe-acd0-4e98-b3e7-fbabbf8b8ef5"; // Orewa
+      getSKUs();
+      async function getSKUs() {
+        const fetchNewWorldSkus: Response = await fetch(
+          `https://api-prod.newworld.co.nz/v1/edge/search/products/query/index/products-index-popularity-asc`,
+          {
+            method: "post",
+            headers: new Headers({
+              "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+              Authorization: newworldSecretToken,
+              "Content-Type": "application/json",
+            }),
+            body: JSON.stringify({
+              algoliaQuery: {
+                attributesToRetrieve: ["productID", "Type", "sponsored"],
+                filters: `stores:${storeID}`,
+                query: searchTerm,
+              },
+            }),
+          }
+        );
+
+        !fetchNewWorldSkus.ok
+          ? setNewworldAPIStatus("API key needs refreshing")
+          : setNewworldAPIStatus("");
+        const newworldResponse = await fetchNewWorldSkus.json();
+        setNewworldProductSKUs(newworldResponse.hits);
       }
-    );
-
-    !fetchNewWorldSkus.ok
-      ? setNewworldAPIStatus("API key needs refreshing")
-      : setNewworldAPIStatus("");
-    const newworldResponse = await fetchNewWorldSkus.json();
-    console.log(newworldResponse.hits);
-
-    setNewworldProductIDs(newworldResponse.hits);
-
-    let productIdTogether: string[] = [];
-    newworldProductIDs !== undefined &&
-      newworldProductIDs.forEach((product) => {
-        productIdTogether.push(product["productID"]);
-      });
-
-    // Send skus to get products
-
-    const combineNewworldSKUsWithProducts: Response = await fetch(
-      `https://api-prod.newworld.co.nz/v1/edge/store/0f82d3fe-acd0-4e98-b3e7-fbabbf8b8ef5/decorateProducts`,
-      {
-        method: "post",
-        headers: new Headers({
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-          Authorization: newworldSecretToken,
-          "Content-Type": "application/json",
-        }),
-        body: JSON.stringify({ productIds: productIdTogether }),
-      }
-    );
-    const newworldSKUsJSON = await combineNewworldSKUsWithProducts.json();
-    newworldSKUsJSON;
-    console.log(newworldSKUsJSON.products);
-    setnewworldResults(newworldSKUsJSON.products);
+    }
   }
 
-  newworldResults !== undefined &&
-    newworldResults.forEach((product, countdownIndex) => {
-      // console.log(product);
-      const productName = product["brand"]
-        ? `${product["brand"]} ${product["name"]}`
-        : product["name"];
-      if (
-        productName
-          .toLowerCase()
-          .replace("-", " ")
-          .includes(searchTerm.toLowerCase())
-      ) {
-        const store = "New World";
-        const productStandardPrice = (
-          product["singlePrice"]["price"] / 100
-        ).toFixed(2);
-
-        const productSpecialPrice = product["promotions"]
-          ? (product["promotions"][0]["rewardValue"] / 100).toFixed(2)
-          : productStandardPrice;
-        const productSku = product["productId"];
-
-        const productCupPrice = product["singlePrice"]["comparativePrice"]
-          ? product["singlePrice"]["comparativePrice"]["pricePerUnit"]
-          : "";
-
-        const productCupUnit = product["singlePrice"]["comparativePrice"]
-          ? product["singlePrice"]["comparativePrice"]["unitQuantity"]
-          : "";
-        const productCupMeasure = product["singlePrice"]["comparativePrice"]
-          ? product["singlePrice"]["comparativePrice"]["unitQuantityUom"]
-          : "";
-
-        const ratio =
-          productCupMeasure &&
-          `$${(productCupPrice / 100).toFixed(
-            2
-          )} / ${productCupUnit} ${productCupMeasure
-            ?.replace("l", "L")
-            ?.replace("mL", "ml")}`;
-
-        const productPackage = `${product["displayName"]
-          ?.replace("l", "L")
-          ?.replace("mL", "ml")}`;
-        const URL = `https://www.newworld.co.nz/shop/product/${productSku?.replace(
-          "-",
-          "_"
-        )}`;
-        const onSpecial = product["promotions"] && true;
-        rows.push(
-          createData(
-            countdownIndex,
-            CapitalizeFirstLetter(productName),
-            onSpecial,
-            productSpecialPrice,
-            productStandardPrice,
-            productPackage,
-            ratio,
-
-            store,
-            URL
-          )
-        );
-      }
-      console.log(productName.toLowerCase());
-      console.log(searchTerm.toLowerCase());
-      console.log(rows);
-    });
+  // Send skus to get products
 
   //   // const paknsaveStoreId = "64eab5b1-8d79-45f4-94f1-02b8cf8b6202"; // Silverdale
   //   // const fetchPaknSaveData = fetch(
@@ -533,133 +541,6 @@ export default function EnhancedTable() {
     );
   };
 
-  const top100Films = [
-    { title: "The Shawshank Redemption", year: 1994 },
-    { title: "The Godfather", year: 1972 },
-    { title: "The Godfather: Part II", year: 1974 },
-    { title: "The Dark Knight", year: 2008 },
-    { title: "12 Angry Men", year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: "Pulp Fiction", year: 1994 },
-    {
-      title: "The Lord of the Rings: The Return of the King",
-      year: 2003,
-    },
-    { title: "The Good, the Bad and the Ugly", year: 1966 },
-    { title: "Fight Club", year: 1999 },
-    {
-      title: "The Lord of the Rings: The Fellowship of the Ring",
-      year: 2001,
-    },
-    {
-      title: "Star Wars: Episode V - The Empire Strikes Back",
-      year: 1980,
-    },
-    { title: "Forrest Gump", year: 1994 },
-    { title: "Inception", year: 2010 },
-    {
-      title: "The Lord of the Rings: The Two Towers",
-      year: 2002,
-    },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: "Goodfellas", year: 1990 },
-    { title: "The Matrix", year: 1999 },
-    { title: "Seven Samurai", year: 1954 },
-    {
-      title: "Star Wars: Episode IV - A New Hope",
-      year: 1977,
-    },
-    { title: "City of God", year: 2002 },
-    { title: "Se7en", year: 1995 },
-    { title: "The Silence of the Lambs", year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: "Life Is Beautiful", year: 1997 },
-    { title: "The Usual Suspects", year: 1995 },
-    { title: "Léon: The Professional", year: 1994 },
-    { title: "Spirited Away", year: 2001 },
-    { title: "Saving Private Ryan", year: 1998 },
-    { title: "Once Upon a Time in the West", year: 1968 },
-    { title: "American History X", year: 1998 },
-    { title: "Interstellar", year: 2014 },
-    { title: "Casablanca", year: 1942 },
-    { title: "City Lights", year: 1931 },
-    { title: "Psycho", year: 1960 },
-    { title: "The Green Mile", year: 1999 },
-    { title: "The Intouchables", year: 2011 },
-    { title: "Modern Times", year: 1936 },
-    { title: "Raiders of the Lost Ark", year: 1981 },
-    { title: "Rear Window", year: 1954 },
-    { title: "The Pianist", year: 2002 },
-    { title: "The Departed", year: 2006 },
-    { title: "Terminator 2: Judgment Day", year: 1991 },
-    { title: "Back to the Future", year: 1985 },
-    { title: "Whiplash", year: 2014 },
-    { title: "Gladiator", year: 2000 },
-    { title: "Memento", year: 2000 },
-    { title: "The Prestige", year: 2006 },
-    { title: "The Lion King", year: 1994 },
-    { title: "Apocalypse Now", year: 1979 },
-    { title: "Alien", year: 1979 },
-    { title: "Sunset Boulevard", year: 1950 },
-    {
-      title:
-        "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-      year: 1964,
-    },
-    { title: "The Great Dictator", year: 1940 },
-    { title: "Cinema Paradiso", year: 1988 },
-    { title: "The Lives of Others", year: 2006 },
-    { title: "Grave of the Fireflies", year: 1988 },
-    { title: "Paths of Glory", year: 1957 },
-    { title: "Django Unchained", year: 2012 },
-    { title: "The Shining", year: 1980 },
-    { title: "WALL·E", year: 2008 },
-    { title: "American Beauty", year: 1999 },
-    { title: "The Dark Knight Rises", year: 2012 },
-    { title: "Princess Mononoke", year: 1997 },
-    { title: "Aliens", year: 1986 },
-    { title: "Oldboy", year: 2003 },
-    { title: "Once Upon a Time in America", year: 1984 },
-    { title: "Witness for the Prosecution", year: 1957 },
-    { title: "Das Boot", year: 1981 },
-    { title: "Citizen Kane", year: 1941 },
-    { title: "North by Northwest", year: 1959 },
-    { title: "Vertigo", year: 1958 },
-    {
-      title: "Star Wars: Episode VI - Return of the Jedi",
-      year: 1983,
-    },
-    { title: "Reservoir Dogs", year: 1992 },
-    { title: "Braveheart", year: 1995 },
-    { title: "M", year: 1931 },
-    { title: "Requiem for a Dream", year: 2000 },
-    { title: "Amélie", year: 2001 },
-    { title: "A Clockwork Orange", year: 1971 },
-    { title: "Like Stars on Earth", year: 2007 },
-    { title: "Taxi Driver", year: 1976 },
-    { title: "Lawrence of Arabia", year: 1962 },
-    { title: "Double Indemnity", year: 1944 },
-    {
-      title: "Eternal Sunshine of the Spotless Mind",
-      year: 2004,
-    },
-    { title: "Amadeus", year: 1984 },
-    { title: "To Kill a Mockingbird", year: 1962 },
-    { title: "Toy Story 3", year: 2010 },
-    { title: "Logan", year: 2017 },
-    { title: "Full Metal Jacket", year: 1987 },
-    { title: "Dangal", year: 2016 },
-    { title: "The Sting", year: 1973 },
-    { title: "2001: A Space Odyssey", year: 1968 },
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: "Toy Story", year: 1995 },
-    { title: "Bicycle Thieves", year: 1948 },
-    { title: "The Kid", year: 1921 },
-    { title: "Inglourious Basterds", year: 2009 },
-    { title: "Snatch", year: 2000 },
-    { title: "3 Idiots", year: 2009 },
-    { title: "Monty Python and the Holy Grail", year: 1975 },
-  ];
   return (
     <>
       <Box
@@ -711,7 +592,7 @@ export default function EnhancedTable() {
                   setSearchHelperText("Please search for something");
                 } else {
                   e.preventDefault();
-                  searchTerm != "" && GetData();
+                  searchTerm != "" && GetSupermarketPrices();
                   setSearchPlaceholderText("Search for a product");
                   setSearchHelperText("");
                 }
@@ -727,7 +608,7 @@ export default function EnhancedTable() {
               if (searchTerm === "") {
                 setSearchHelperText("Please search for something");
               } else {
-                GetData();
+                GetSupermarketPrices();
                 setSearchPlaceholderText(searchTerm);
                 setSearchHelperText("");
               }
