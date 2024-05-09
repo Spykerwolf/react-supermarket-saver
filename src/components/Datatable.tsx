9c7a05ffa73a044fc80740f79c69e08b1790481d
Spykerwolf <willsmal.nz@gmail.com>
2024-05-09 12:39:42 +1200

    Filter with filter button

--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -997,7 +997,20 @@ export default function EnhancedTable() {
     }
   }
 
-  function handleFilter(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleFilterButtonClick(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    if (!existingTag) {
+      setTags([...tags, filterSearchText]);
+      setFilterSearchText("");
+      e.preventDefault;
+    } else if (existingTag) {
+      setFilterSearchText("");
+      e.preventDefault;
+    }
+  }
+
+  function handleSetFilterSearchText(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.value !== " ") {
       e.preventDefault;
       e.target.value !== "," &&
@@ -1060,24 +1073,26 @@ export default function EnhancedTable() {
               placeholder="Filter a product"
               value={filterSearchText.replace(",", "")}
               onKeyDown={handleFilterCommaOrEnterKey}
-              onChange={handleFilter}
+              onChange={handleSetFilterSearchText}
             />
-            <Button
-              id="filterButton"
-              variant="contained"
-              size="small"
-              endIcon={<RemoveIcon />}
-              onClick={() => setFilterSearchText("")}
-              type="button"
-              sx={{
-                marginBottom: "5px",
-                paddingRight: "20px",
-                height: "42px",
-              }}
-              aria-label="search"
-            >
-              Filter
-            </Button>
+            <Tooltip title="Filter with comma or enter">
+              <Button
+                id="filterButton"
+                variant="contained"
+                size="small"
+                endIcon={<RemoveIcon />}
+                onClick={handleFilterButtonClick}
+                type="button"
+                sx={{
+                  marginBottom: "5px",
+                  paddingRight: "20px",
+                  height: "42px",
+                }}
+                aria-label="search"
+              >
+                Filter
+              </Button>
+            </Tooltip>
           </ButtonGroup>
 
           {tags.length > 0 && (
